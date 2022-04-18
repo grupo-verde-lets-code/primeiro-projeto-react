8e22964efcd9dea114cb6d1f47fef33c803be572
Altair Feroli <altair.feroli@gmail.com>
2022-04-18 15:37:53 -0300

    Estilização da página de cadastro
    - Tamanhos e estilos de fonte
    - Botão de cadastro
    - Sombras

--- primeiro-projeto-react/src/pages/Register/index.jsx
+++ primeiro-projeto-react/src/pages/Register/index.jsx
@@ -3,35 +3,35 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 
-/* import "../../Styles/css/Register.css"; */
+import "../../Styles/css/Register.css";
 
 const Register = () => {
   return (
     <>
       <Container>
         <Form className="form">
-          <Form.Group className="mb-3" controlId="formBasicTitle">
-            <Form.Label>Nome</Form.Label>
+          <Form.Group className="mb-3 form-group" controlId="formBasicTitle">
+            <Form.Label className="form-label">Nome</Form.Label>
             <Form.Control className="form-control" placeholder="Digite o nome do produto" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPrice">
-            <Form.Label>Preço</Form.Label>
+          <Form.Group className="mb-3 form-group" controlId="formBasicPrice">
+            <Form.Label className="form-label">Preço</Form.Label>
             <Form.Control className="form-control" placeholder="Digite o Preço" />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicInfos">
-            <Form.Label>Informações/Descrição</Form.Label>
+          <Form.Group className="mb-3 form-group" controlId="formBasicInfos">
+            <Form.Label className="form-label">Informações/Descrição</Form.Label>
             <Form.Control className="form-control" as="textarea" rows={5} placeholder="Descreva seu produto"></Form.Control>
           </Form.Group>
-          <Form.Group classname="mb-3" controlId="formBasicWeight">
-            <Form.Label>Peso</Form.Label>
+          <Form.Group classname="mb-3 form-group" controlId="formBasicWeight">
+            <Form.Label className="form-label">Peso</Form.Label>
             <Form.Control className="form-control" placeholder="Digite o peso"></Form.Control>
           </Form.Group>
-          <Form.Group controlId="formFile" className="mb-3">
-            <Form.Label>Imagem do produto</Form.Label>
+          <Form.Group controlId="formFile" className="mb-3 form-group">
+            <Form.Label className="form-label">Imagem do produto</Form.Label>
             <Form.Control className="form-control" type="file" />
           </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
+          <Button className="submit-button" variant="primary" type="submit">
+            Cadastrar
           </Button>
         </Form>
       </Container>
--- primeiro-projeto-react/src/pages/_templates/Default/index.jsx
+++ primeiro-projeto-react/src/pages/_templates/Default/index.jsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
 
 export const DefaultTemplate = (props) => {
   return (
     <>
       <Navbar className="navbar" bg="primary" variant="light">
         <Container>
-          <Navbar.Brand className="navbar-brand" href="/">E-Commerce</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/registrar">Registrar</Nav.Link>
+          <Navbar.Brand className="navbar-brand" href="/">
+            E-Commerce
+          </Navbar.Brand>
+          <Nav className="ml-auto">
+            <Nav.Link className="navbar-link" href="/">
+              Home
+            </Nav.Link>
+            <Nav.Link className="navbar-link" href="/registrar">
+              Registrar
+            </Nav.Link>
           </Nav>
         </Container>
       </Navbar>
